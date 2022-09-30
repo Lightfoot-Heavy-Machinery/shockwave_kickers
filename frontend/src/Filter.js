317388f371d5d4c9a352e8dda904d36882c7c059
Stuart N <stuartnelson98@gmail.com>
2022-09-30 12:54:13 -0700

    yuh

---
+++ frontend/src/Filter.js
@@ -0,0 +1,14 @@
+/*
+import React, { useState } from "react";
+import "./Filter.css";
+
+function Filter() {
+    const [filter, setFilter] = useState("");
+    console.log(filter);
+    return (
+    
+    )
+}
+
+export default Filter
+*/
--- frontend/src/View.js
+++ frontend/src/View.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "./Header";
 import Table from "./Table";
+//import Filter from "./Filter";
 
 function View() {
     return (
