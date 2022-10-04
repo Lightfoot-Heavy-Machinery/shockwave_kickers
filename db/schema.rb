3d4ef0bcea5a9901c9423b3d3594b1f2c852b204
kunalvudathu <kunal_vudathu@trendmicro.com>
2022-10-03 20:33:46 -0500

    add linux as platform supported

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_09_27_195235) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "courses", force: :cascade do |t|
     t.string "course_number"
     t.string "sections"
