eec3c181fff72a9be2c12a170f3dd5b4b8da8e64
Rahul  Shah <rahulshah@client-10-228-19-142.tamulink.tamu.edu>
2022-10-04 13:39:25 -0500

    add semester controller

---
+++ app/controllers/semesters_controller.rb
@@ -0,0 +1,38 @@
+class SemestersController < ApplicationController
+  def index
+      @semesters = Semester.all
+  end
+
+  def show
+      @semester = Semester.find(params[:id])
+  end
+
+  def new
+      @semesters = Semester.all
+      @semester = Semester.find(params[:id])
+  end
+
+  def edit
+      @semester = Semester.find(params[:id])
+  end
+
+  def create
+      semester = Semester.new(params[:semester].permit(:name, :courses))
+      if semester.save
+          redirect_to new_semester_path
+      end
+  end
+
+  def update
+    @semester = Semester.find(params[:id])
+    respond_to do |format|
+      if @semester.update(params.require(:semester).permit(:name, :courses))
+        format.html { redirect_to semester_url(@semester), notice: "Semester information was successfully updated." }
+        format.json { render :show, status: :ok, location: @semester }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @semester.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'semesters/index'
+  get 'semesters/show'
+  get 'semesters/new'
+  get 'semesters/edit'
   resources :courses, :students
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
