3cde9725f276eb92584534df22a1fdaaf7216998
Rahul  Shah <rahulshah@client-10-228-19-142.tamulink.tamu.edu>
2022-10-04 13:46:55 -0500

    update schema and routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get 'semesters/show'
   get 'semesters/new'
   get 'semesters/edit'
-  resources :courses, :students
+  resources :courses, :students, :semesters
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
