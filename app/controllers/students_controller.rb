137b99afc8857d34383991afbd5def0741780fe6
kunalvudathu <kunal_vudathu@trendmicro.com>
2022-10-20 18:03:18 -0500

    Enable authentication

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,4 +1,5 @@
 class StudentsController < ApplicationController
+    before_action :authenticate_user!
     before_action :set_student, only: %i[ show edit update destroy ]
     # GET /students
     def index
---
+++ db/migrate/20221020212001_drop_users_foreign_key.rb
@@ -0,0 +1,5 @@
+class DropUsers < ActiveRecord::Migration[7.0]
+  def change
+    remove_foreign_key :courses, column: :teacher
+  end
+end
