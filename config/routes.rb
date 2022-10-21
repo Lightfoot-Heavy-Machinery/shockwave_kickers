30e1e970e4b9a8aa290863f582a3ac2eff921a93
jkelly8823 <jkelly8823@gmail.com>
2022-10-21 13:45:25 -0500

    Added upload/ route and sample quiz page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'upload/new'
   get 'upload/index'
+  get 'upload', to: 'upload#index'
   resources :courses, :students
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
