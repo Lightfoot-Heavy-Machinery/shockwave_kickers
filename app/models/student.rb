259874e272d18127d0deed0d4b778546cb8ebe90
kunalvudathu <kunal_vudathu@trendmicro.com>
2022-11-01 22:36:10 -0500

    final changes

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,5 @@
 class Student < ApplicationRecord
     has_one_attached :image do |attachable|
-        attachable.variant :thumb, resize_to_limit: [100, 100]
+        attachable.variant :thumb, resize_to_fill: [500, 500]
     end
 end
