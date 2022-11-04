7a0f36ac1c1bb660e5cd15f25097fa824b4c438b
jkelly8823 <jkelly8823@gmail.com>
2022-11-04 10:46:14 -0500

    rm public/assets

--- public/assets/application-e73a1ca97fdd6300c88f011f9b6091858c676698ee575e10e40eba91bface222.js
+++
@@ -1,8 +0,0 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import { Turbo } from "@hotwired/turbo-rails"
-Turbo.session.drive = false
-import "controllers"
-
-//= require jquery3
-//= require popper
-//= require bootstrap-sprockets;
