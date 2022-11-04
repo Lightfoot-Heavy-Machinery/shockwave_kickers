3839d0375f04d88e8b82e9041156506249dda383
jkelly8823 <jkelly8823@gmail.com>
2022-11-04 11:09:35 -0500

    Removed some unneeded controller & view elements

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -58,10 +58,6 @@ class QuizzesController < ApplicationController
     @quiz = Quiz.new
   end
 
-  # GET /quizzes/1/edit
-  def edit
-  end
-
   # POST /quizzes or /quizzes.json
   def create
     @quiz = Quiz.new(quiz_params)
@@ -76,29 +72,6 @@ class QuizzesController < ApplicationController
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @quiz.errors, status: :unprocessable_entity }
       end
-    end
-  end
-
-  # PATCH/PUT /quizzes/1 or /quizzes/1.json
-  def update
-    respond_to do |format|
-      if @quiz.update(quiz_params)
-        format.html { redirect_to quiz_url(@quiz), notice: "Quiz was successfully updated." }
-        format.json { render :show, status: :ok, location: @quiz }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @quiz.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /quizzes/1 or /quizzes/1.json
-  def destroy
-    @quiz.destroy
-
-    respond_to do |format|
-      format.html { redirect_to quizzes_url, notice: "Quiz was successfully destroyed." }
-      format.json { head :no_content }
     end
   end
 
