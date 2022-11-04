db67ba68afd8cce54c01b6eedca5cc4ff45595c8
Rahul  Shah <rahulshah@client-10-229-101-182.tamulink.tamu.edu>
2022-11-04 14:49:59 -0500

    fix merge conflicts(

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,11 @@ Rails.application.routes.draw do
 
   devise_for :users
 
-  resources :courses, :students
+  resources :courses, :students do
+      collection do
+          get 'list'
+      end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
