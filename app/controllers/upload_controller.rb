effee76cba9c67cb0d40db66b09e52d3b946a3ef
kunalvudathu <92550453+kunalvudathu@users.noreply.github.com>
2022-11-11 13:43:25 -0600

    Real people and pictures (#47)

--- app/controllers/upload_controller.rb
+++ app/controllers/upload_controller.rb
@@ -5,43 +5,39 @@ class UploadController < ApplicationController
         require 'json'
         require 'securerandom'
 
-        image_paths = []
-        # image_path = "./app/resources/pictures/test.jpeg"
-        image_path = "./app/resources/pictures/test"
+        image_path_default = "./app/resources/pictures/test.jpeg"
+        image_path = "./app/resources/pictures/"
         image_ext = ".jpeg"
-        (0..20).each do |v|
-            if v == 0
-                image_paths.append(image_path+""+image_ext)
-            else
-                image_paths.append(image_path+v.to_s+image_ext)
-            end
-        end
 
-        count = 0
+        course_ids = Course.where("course_name like ?", "%#{"CSCE 606"}%").pluck(:id)
+        @students = Student.where(course_id: course_ids)
+        @students.each do |s|
+            s.image.purge_later
+            s.destroy
+        end
 
         CSV.foreach('./app/resources/test_data.csv', :headers => true) do |record|
             uuid = SecureRandom.uuid
 
-            @course = Course.find_or_create_by(course_name: record["Course"], teacher: current_user.email, section: record["Section"], semester: record["Semester"])
+            @course = Course.find_or_create_by(course_name: record["Course"].strip(), teacher: current_user.email, section: record["Section"].strip(), semester: record["Semester"].strip())
             @student = Student.find_or_create_by(
-                        firstname:record["FirstName"],
-                        lastname:record["LastName"],
-                        uin: record["UIN"],
-                        email: record["Email"],
-                        classification: record["Classification"],
-                        major: record["Major"],
-                        notes: record["Notes"],
+                        firstname:record["FirstName"].strip(),
+                        lastname:record["LastName"].strip(),
+                        uin: record["UIN"].strip(),
+                        email: record["Email"].strip(),
+                        classification: record["Classification"].strip(),
+                        major: record["Major"].strip(),
+                        notes: record["Notes"].strip(),
                         course_id: @course.id,
                         teacher: current_user.email
                         )
-            
-            # @student.image.attach(io: File.open(image_path, 'rb'), filename: uuid)
-            if count < image_paths.length()
-                @student.image.attach(io: File.open(image_paths[count], 'rb'), filename: uuid)
+
+            file_path = image_path + record["FirstName"] + image_ext
+            if File.exist?(file_path)
+                @student.image.attach(io: File.open(file_path, 'rb'), filename: uuid)
             else
-                @student.image.attach(io: File.open(image_paths[0], 'rb'), filename: uuid)
+                @student.image.attach(io: File.open(image_path_default, 'rb'), filename: uuid)
             end
-            count = count + 1
 
             if !@student.save
                 puts("failed to save student")
