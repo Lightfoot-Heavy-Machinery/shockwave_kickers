d735f79870fd5a10d207320f76933611da2556b1
kunalvudathu <kunal_vudathu@trendmicro.com>
2022-11-20 20:25:53 -0600

    remove not used code

--- app/lib/StudentCourseEntry.rb
+++ app/lib/StudentCourseEntry.rb
@@ -1,8 +1,3 @@
 class StudentCourseEntry
     attr_accessor :student_record, :course_record
-
-    def initializeUsingStudentModel student, course
-        @records = student
-        @course_record = course
-    end
 end
