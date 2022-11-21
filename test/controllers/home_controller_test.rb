2bc32abd766aa6a1d458162aad38b4563ccb63c6
jkelly8823 <jkelly8823@gmail.com>
2022-11-20 22:20:47 -0600

    Home Tests

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,16 +1,47 @@
 class HomeControllerTest < ActionDispatch::IntegrationTest
     setup do
-      sign_in users(:userOne)
     end
   
-    test "should get home index" do
+    test "should get home index u1" do
+      sign_in users(:userOne)
+      get '/home/index'
+      assert_response :success
+    end
+
+    test "should get home index u2" do
+      sign_in users(:userTwo)
+      get '/home/index'
+      assert_response :success
+    end
+
+    test "should get home index u3" do
+      sign_in users(:userThree)
+      get '/home/index'
+      assert_response :success
+    end
+
+    test "should get home index u4" do
+      sign_in users(:userFour)
+      get '/home/index'
+      assert_response :success
+    end
+
+    test "should get home index u5" do
+      sign_in users(:userFive)
+      get '/home/index'
+      assert_response :success
+    end
+
+    test "should get home index u6" do
+      sign_in users(:userSix)
       get '/home/index'
       assert_response :success
     end
 
     test "should redirect home index" do
-        sign_out users(:userOne)
-        get '/home/index'
-        assert_redirected_to '/users/sign_in'
+      sign_in users(:userOne)
+      sign_out users(:userOne)
+      get '/home/index'
+      assert_redirected_to '/users/sign_in'
     end
 end
