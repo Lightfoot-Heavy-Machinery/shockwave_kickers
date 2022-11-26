a3aa38f26b4a83d3076f632e92b21354668ebace
kunalvudathu <kunal_vudathu@trendmicro.com>
2022-11-26 14:36:49 -0600

    missed changed

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -2,7 +2,7 @@ class Quiz < ApplicationRecord
     validate :has_students
     private
     def has_students
-        cnt = Student.where(teacher: self.teacher, id: StudentCourse.where(course_id:self.course_id)).count.to_i
+        cnt = Student.where(teacher: self.teacher, id: StudentCourse.where(course_id:self.course_id).pluck(:student_id)).count.to_i
         self.errors.clear
         if cnt < 1
             self.errors.add(:base, "Cannot create a quiz from a selection with 0 students!")
