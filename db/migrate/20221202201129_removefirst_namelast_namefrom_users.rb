c2ab2a0e5d4dda5e025777e2ffe27fbb0f33af15
jkelly8823 <jkelly8823@gmail.com>
2022-12-02 14:35:38 -0600

    Remove migration, update schema

--- db/migrate/20221202201129_removefirst_namelast_namefrom_users.rb
+++
@@ -1,6 +0,0 @@
-class RemovefirstNamelastNamefromUsers < ActiveRecord::Migration[7.0]
-  def change
-    remove_column :users, :firstName
-    remove_column :users, :lastName
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -109,8 +109,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_02_072114) do
     t.string "unconfirmed_email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "firstName"
-    t.string "lastName"
     t.string "firstname", default: ""
     t.string "lastname", default: ""
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
