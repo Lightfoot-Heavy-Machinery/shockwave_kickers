d82ffd44de00b74279d2b7c00c5e1ec84b106d88
Stuart N <76634287+thessnelson@users.noreply.github.com>
2022-12-02 11:53:56 -0800

    Uploads (#70)
    * active directory done, merge conflicts fixed
    
    * basic frontend functionality redone
    
    * changed style slightly. still debugging postings.
    
    * still working on the upload pages
    
    * posts
    
    * adding search, debugging upload
    
    * course searchbar complete
    
    * searchbar done!
    
    * fixed typo in courses controller
    
    * uploads and search working. Need to push to s3.
    
    * completed image attachment
    
    * moved posts to upload
    
    * slight debug to upload
    
    * fixed the search issue
    
    * fixed the search error
    
    * added kunal's changes
    
    * added kunahl's changes.
    
    * added kunals changes
    
    Co-authored-by: csimpson2018 <98438242+csimpson2018@users.noreply.github.com>

--- Gemfile
+++ Gemfile
@@ -59,6 +59,10 @@ gem "image_processing", "~> 1.2"
 gem "mini_magick"
 gem 'aws-sdk-s3', require: false
 
+#used for the drag-and-drop file upload feature
+gem 'dropzonejs-rails'
+gem 'rubyzip'
+
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri mingw x64_mingw ]
--- app/models/course.rb
+++ app/models/course.rb
@@ -1,2 +1,16 @@
 class Course < ApplicationRecord
+    def self.search(search, teacher)
+        if search
+          search_type = Course.find_by(course_name: search, teacher: teacher)
+          if search_type
+            self.where(id: search_type)
+          elsif (search.length == 0)
+            @courses_db_result = Course.where(teacher: teacher)
+          else
+            @courses_db_result = Course.where(id: 0)
+          end
+        else
+          @courses_db_result = Course.where(teacher: teacher)
+        end
+    end
 end
