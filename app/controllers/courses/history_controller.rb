d03629c9ed35d37c8a0a5a67cbf0aa0cd91346a2
jkelly8823 <60297161+jkelly8823@users.noreply.github.com>
2022-12-09 18:40:02 -0600

    Updates (#82)

--- app/controllers/courses/history_controller.rb
+++ app/controllers/courses/history_controller.rb
@@ -4,7 +4,7 @@ module Courses
         before_action :set_course, only: %i[ show ]
 
         def show
-            @course_records = Course.where(course_name: Course.where(id: params[:id])[0].course_name)
+            @course_records = Course.where(course_name: Course.where(id: params[:id])[0].course_name, teacher: current_user.email)
         end
 
         private
