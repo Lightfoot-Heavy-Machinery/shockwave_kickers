c2fb3a6ac7eee2416767a767074bd695f700bb25
Ubuntu <ubuntu@ip-172-31-1-56.ap-northeast-1.compute.internal>
2023-01-06 03:25:26 +0000

    added a cucumber test to courses

--- test/controllers/courses_story_test.rb
+++ test/controllers/courses_story_test.rb
@@ -1,3 +1,9 @@
+Given /the following courses exist/ do |courses_table|
+    courses_table.hashes.each do |course|
+      Course.create course
+    end
+end
+
 Given('I am on the courses page')
     visit "/courses"
 end
