46e513bde23a666409c6277416d8e4f34c12a71b
Ubuntu <ubuntu@ip-172-31-1-56.ap-northeast-1.compute.internal>
2023-01-09 04:33:31 +0000

    pruntested Given statements in cucumber files

--- test/controllers/courses_story_test.rb
+++
@@ -1,9 +0,0 @@
-Given /the following courses exist/ do |courses_table|
-    courses_table.hashes.each do |course|
-      Course.create course
-    end
-end
-
-Given('I am on the courses page')
-    visit "/courses"
-end
