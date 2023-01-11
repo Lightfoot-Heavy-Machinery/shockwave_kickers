059196dfa2dcd2b2d8d28b4334e75e2aa5d5ca67
hdhulipala02 <hdhulipala02@tamu.edu>
2023-01-11 03:56:54 +0000

    Revert "added oauth without correct routing"
    This reverts commit 505cd6f3b411a5486e43aa8e588093d2c992a549.

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,8 +9,8 @@ class User < ApplicationRecord
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
-      #user.full_name = auth.info.name
-      #user.avatar_url = auth.info.image
+      user.full_name = auth.info.name
+      user.avatar_url = auth.info.image
       #user.skip_confirmation!
     end
   end
--- db/migrate/20230111023806_add_provider_to_users.rb
+++
@@ -1,6 +0,0 @@
-class AddProviderToUsers < ActiveRecord::Migration[7.0]
-  def change
-    add_column :users, :provider, :string, default: ""
-    add_column :users, :uid, :string, default: ""
-  end
-end
