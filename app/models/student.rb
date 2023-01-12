65295d33d33b59bcaaccb579d004bb89737c9ce1
Ubuntu <ubuntu@ip-172-26-4-26.ap-southeast-1.compute.internal>
2023-01-12 10:09:56 +0000

    commented out broken function

--- app/models/student.rb
+++ app/models/student.rb
@@ -4,19 +4,20 @@ class Student < ApplicationRecord
         attachable.variant :thumb, resize_to_fill: [500, 500]
     end
 
-    def self.search(search, teacher)
-        if search
-          search_type = Student.find_by(email: search, teacher: teacher)
-          if search_type
-            self.where(id: search_type)
-          elsif (search.length == 0)
-            #return no results
-            @students = Student.where(teacher: teacher)
-          else
-            @students = Student.where(id: 0)
-          end
-        else
-          @students = Student.where(teacher: teacher)
-        end
-      end
+    # TO FIX
+    # def self.search(search, teacher)
+    #     if search
+    #       search_type = Student.find_by(email: search, teacher: teacher)
+    #       if search_type
+    #         self.where(id: search_type)
+    #       elsif (search.length == 0)
+    #         #return no results
+    #         @students = Student.where(teacher: teacher)
+    #       else
+    #         @students = Student.where(id: 0)
+    #       end
+    #     else
+    #       @students = Student.where(teacher: teacher)
+    #     end
+    #   end
 end
